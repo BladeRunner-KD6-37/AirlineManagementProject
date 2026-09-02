8ffc99bc9d33905d22e4e356d8a9bcd6a64853e7
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-02 09:31:56 +0530

    added more routers for city api

--- FlightsAndSearch/src/routes/v1/index.js
+++ FlightsAndSearch/src/routes/v1/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const CityControllers = require('../../controllers/city-controller');
+const CityController = require('../../controllers/city-controller');
 
-router.post('/city', CityControllers.create);
-router.delete('/city/:id', CityControllers.destroy);
-router.get('/city/:id',CityControllers.get)
-router.patch('/city/:id', CityControllers.update)
+router.post('/city', CityController.create);
+router.delete('/city/:id', CityController.destroy);
+router.get('/city/:id',CityController.get)
+router.patch('/city/:id', CityController.update)
 
 module.exports = router;
