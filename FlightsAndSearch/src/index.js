dd33626b40ed94fd845671719793cf9581c9cd43
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-02 23:09:26 +0530

    seeders created

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -4,6 +4,10 @@ const { PORT } = require('./config/serverConfig');
 const bodyParser = require('body-parser');
 const ApiRoutes = require('./routes/index')
 
+const { Airport, City }  = require('./models/index');
+const db = require('./models/index');
+const sequelize = require('sequelize');
+
 const setupAndStartServer = async () => {
     try {
         const app = express();
@@ -11,9 +15,48 @@ const setupAndStartServer = async () => {
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use('/api', ApiRoutes);
 
-        app.listen(PORT, () => {
+        app.listen(PORT, async () => {
             console.log(`Server started at ${PORT}`);
+
+            if(process.env.SYNC_DB){
+                db.sequelize.sync({alter : true})
+            }
+
+            // const airports = await City.findAll({
+            //     where : {
+            //         id : 3
+            //     },
+            //     include : [
+            //         {
+            //             model : Airport
+            //         }
+            //     ]
+            // });
+
+            // console.log(airports)
+
+            // db.sequelize.sync({alter: true});
+            // const city = await City.findOne({
+            //     where : {
+            //         id : 3
+            //     }
+            // });
+
+            // const airports =  await city.getAirports();
+
+            // const newAirport = await Airport.findOne({
+            //      where :{
+            //          id : 5
+            //      }
+            // });
+            // await city.addAirport(newAirport);
+            // await city.addAirports({
+            //     name : 'Jindal Vijaynagar airport'
+            // })
+            // console.log(city,airports)
+
         });
+        
     } catch (error) {
         console.error('Failed to start server:', error);
         process.exit(1);
