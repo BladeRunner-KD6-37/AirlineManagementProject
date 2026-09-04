3432ea6261aa4334155511ce8499d6a22135f2c8
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-04 17:43:10 +0530

    created middle ware to validate user input & created a check to avoid bloating req.body

--- FlightsAndSearch/src/routes/v1/index.js
+++ FlightsAndSearch/src/routes/v1/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { FlightMiddlewares} =  require('../../middlewares/index')
 const { FlightController, CityController, AirportController } = require( '../../controllers/index')
 
 router.post('/city', CityController.create);
@@ -9,7 +10,11 @@ router.patch('/city/:id', CityController.update);
 router.get('/city', CityController.getAll);
 
 
-router.post('/flights', FlightController.create);
+router.post(
+        '/flights',
+        FlightMiddlewares.validateCreateFlight,
+        FlightController.create
+    );
 router.get('/flights', FlightController.getAll);
 
 router.post('/airports',AirportController.create);
