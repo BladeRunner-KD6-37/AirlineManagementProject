c18f7ad8b978f721a56a1fbe65618cc200590da7
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-05 23:33:59 +0530

    Setup bcrypt

--- AuthService/src/config/serverConfig.js
+++ AuthService/src/config/serverConfig.js
@@ -3,5 +3,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-    PORT : process.env.PORT
+    PORT : process.env.PORT,
+    SALT : Number.parseInt(process.env.SALT, 10) // need to pass an integer, otherwise it gets converted into a string.
 }
