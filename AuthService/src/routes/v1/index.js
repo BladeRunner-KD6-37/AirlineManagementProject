2ca7d42064295fefdb381d2f027ccb09e1607d1c
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-06 22:11:35 +0530

    added email and password validation for signup, login request

--- AuthService/src/routes/v1/index.js
+++ AuthService/src/routes/v1/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const UserController =  require('../../controllers/user-controller');
+const  { AuthRequestValidator } =  require('../../middlewares/index')
 
-router.post('/signup', UserController.create);
-router.post('/signIn',UserController.signIn );
+router.post('/signup', 
+    AuthRequestValidator.validateUserAuth,
+    UserController.create);
+
+
+router.post('/signIn',
+    AuthRequestValidator.validateUserAuth,
+    UserController.signIn );
 
 
 module.exports =  router;
