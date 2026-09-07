3d0e6c5cfe2ea9714e8d98ac157887a61aedcf16
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-07 21:40:24 +0530

    initial Booking service setup complete

---
+++ BookingService/src/index.js
@@ -0,0 +1,22 @@
+const express =  require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const  { PORT, DB_SYNC } =  require('./config/serverConfig');
+const db= require('./models/index');
+const apiRoutes = require('./routes/index');
+
+const setupAndStartServer = async()=>{
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended : true}));
+    app.use('/api',apiRoutes);
+
+    app.listen(PORT, ()=>{
+        console.log(`Server listening on port ${PORT}`);
+        if(!DB_SYNC){
+            db.sequelize.sync({alter : true})
+        }
+    })
+}
+
+setupAndStartServer();
