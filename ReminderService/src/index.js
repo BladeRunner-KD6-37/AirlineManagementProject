7319b3fd982047dfeee7e1cdee9164edc6f8719e
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-08 23:44:45 +0530

    small changes in reminderservice/src/index.js

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -1,6 +1,6 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
-const  { sendBasicEmail } = require('./services/email-service');
+// const  { sendBasicEmail } = require('./services/email-service');
 
 const  { PORT }= require('./config/serverConfig')
 
@@ -12,12 +12,12 @@ const setupAndStartServer = async()=>{
     
     app.listen(PORT, ()=>{
         console.log(`Server listening on port ${PORT}`); 
-        sendBasicEmail(
-            'support@admin.com',
-            'ayushcantcode90@gmail.com',
-            'This is a testing email',
-            'Hey, how are you. I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'ayushcantcode90@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you. I hope you like the support'
+        // )
     });
 }
 setupAndStartServer();
