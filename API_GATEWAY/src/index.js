e44edc4a95904893ebee3a5e168803aac9044166
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-11 17:01:22 +0530

    Finished with API gateways

--- API_GATEWAY/src/index.js
+++ API_GATEWAY/src/index.js
@@ -1,29 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const rateLimit = require('express-rate-limit') ;
-const { PORT } = require('./config.js/serverConfig') ;
+const rateLimit = require('express-rate-limit');
+const { PORT } = require('./config.js/serverConfig');
+const axios = require('axios');
 
 
-const limiter =  rateLimit({ // rate limiter
-    windowMs : 2 * 60 * 1000,
-    max : 5,
-})
+const limiter = rateLimit({ // rate limiter
+    windowMs: 2 * 60 * 1000,
+    max: 5,
+});
 
 
 const app = express();
 
-app.use(morgan('combined')) ;
-app.use(limiter)
-app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin : true})) ;
+app.use(morgan('combined'));
+app.use(limiter);
+app.use('/bookingservice',async (req, res, next) => {
+    console.log(req.headers['x-access-token']);
+    
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isauthenticated',{
+            headers : {
+                'x-access-token' : req.headers['x-access-token']
+            }
+        });
+        console.log(response.data);
+        if(response.data.success){
+            next ();
+        } else {
+             return res.status(401).json({
+            message : "Unauthorized"
+        })
+        }
+        
+    } catch (error) {
+        return res.status(401).json({
+            message : "Something went wrong"
+        })
+    }
 
-app.get('/home', (req,res)=>{
-    return res.json({
-        message : 'Ok'
-    })
 })
 
+app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 
-app.listen(PORT,()=>{
-        console.log(`Sever listening on port ${PORT}`) ;
+app.get('/home', (req, res) => {
+    return res.json({
+        message: 'Ok'
     });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Sever listening on port ${PORT}`);
+});
