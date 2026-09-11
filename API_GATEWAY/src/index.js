9c62e844d1103c7c6ac05fea6db4f95dc4da063f
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-09-11 12:16:51 +0530

    added rate limiter and port in dotenv

--- API_GATEWAY/src/index.js
+++ API_GATEWAY/src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const PORT = 3005 ;
+const rateLimit = require('express-rate-limit') ;
+const { PORT } = require('./config.js/serverConfig') ;
 
+
+const limiter =  rateLimit({ // rate limiter
+    windowMs : 2 * 60 * 1000,
+    max : 5,
+})
 
 
 const app = express();
 
 app.use(morgan('combined')) ;
+app.use(limiter)
 app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin : true})) ;
 
 app.get('/home', (req,res)=>{
